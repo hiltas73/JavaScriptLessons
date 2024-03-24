b95bdad961ab784208f21d7a6b1ce47ebe3f345f
hiltas73 <h_iltas@yahoo.com>
2024-03-24 18:20:30 +0300

    max value done

--- 12_InterviewTasks.js
+++ 12_InterviewTasks.js
@@ -2,10 +2,18 @@
  * Write a function that can find the max number from a number Array
  */
 
-let numbers = [3, 4, 6, 9, 1, 2, 25, 18, 'hello'];
+let numbers = [3, 4, 6, 9, 1, 2, 20, 180, 'hello'];
 numbers.push(15.5);
 console.log(numbers);
+maxValue(numbers);
 
 function maxValue(arrName) {
+    let maxValue = arrName[0]
+    arrName.forEach(element => {
+        if (maxValue<element) {
+            maxValue = element
+        }
+    });
 
+    console.log(maxValue)
 }
